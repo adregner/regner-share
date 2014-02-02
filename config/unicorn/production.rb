7845f532fad44a9aa24275e6e5841ac0215794c4
Andrew Regner <andrew@aregner.com>
2014-02-02 16:50:04 -0500

    kill old master unicorn

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -15,6 +15,17 @@ stdout_path "log/unicorn.log"
 pid "#{app_path}/tmp/unicorn.pid"
 
 
+before_fork do |server, worker|
+  old_pid = "#{app_path}/tmp/unicorn.pid.oldbin"
+  if File.exists?(old_pid) and server.pid != old_pid
+    begin
+      Process.kill("QUIT", File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
+    end
+  end
+end
+
 __END__
 # Environments
 rails_env
